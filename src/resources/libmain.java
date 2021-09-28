c3b7dd7ea5be92ef8a9ad54aa4a94dbd602acf90
subham <sshoobham10@gmail.com>
2021-09-28 13:46:48 +0545

    commits for libmain and new sets of codes

---
+++ src/resources/libmain.java
@@ -0,0 +1,55 @@
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.*;
+
+public class LibMain {
+    static JFrame frame;
+
+    public static void main(String[] args) {
+        AdminFunctions.refresh();
+        mainMenu();
+    }
+
+    public static void mainMenu() {
+        String[] options = { "Login", "Add new user", "Exit" };
+
+        newFrame("Welcome to the Library", 800, 700);
+        frame.setLayout(null);
+
+        JLabel background = new JLabel(
+                new ImageIcon("C:\\Users\\Dell\\eclipse-workspace\\LibManagement\\src\\resources\\back1.png"));
+        JLabel labelT = new JLabel("Welcome to the Library");
+        labelT.setHorizontalAlignment(JLabel.CENTER);
+        labelT.setBounds(160, 20, 450, 40);
+        labelT.setFont(new Font("Arial", Font.BOLD, 40));
+        labelT.setForeground(Color.black);
+        labelT.setBorder(BorderFactory.createLineBorder(Color.black));
+        frame.add(labelT);
+
+        JLabel made = new JLabel("Made by : Aditya Yadav");
+        made.setFont(new Font("Arial", Font.BOLD, 20));
+        made.setForeground(new Color(102 ,0 ,153));
+        made.setBounds(500, 600, 450, 40);
+
+        frame.add(made);
+        background.setBounds(0, 0, 800, 700);
+        frame.add(background);
+        frame.setSize(799, 699);
+        frame.setSize(800, 700);
+
+        int selection = JOptionPane.showOptionDialog(frame, "Welcome to Library Management System", "Select an option",
+                JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
+
+        if (selection == 0) {
+            LogAdd.login();
+        } else if (selection == 1) {
+            LogAdd.addUser();
+        } else if (selection == 2) {
+            frame.setVisible(false);
+            frame.dispose();
+            System.exit(0);
+
+        }
+
+    }
